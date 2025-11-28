efc7100e9b091709c5d155a9dcc29455ec155dbb
Janis Papewalis <janis.papewalis@alumni.fh-aachen.de>
2025-11-28 13:18:19 +0100

    Bee population Hamilton angelegt und Schritt 1 auf S.8 von Gruene implementiert

--- bee_population.py
+++ bee_population.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 # death and work rates respectively
@@ -21,3 +22,45 @@ T = 365
 # This way we model the times of the year with the summer being the most fruitful.
 def prod(t):
     return 1 + np.cos((2 * t - 365) * np.pi / 365)
+
+# rhs function for the ODE governing x
+def f(t, x, u, s = prod):
+    w, q = x
+    return np.array([
+        -mu * w + b * s(t) * u(t) * w,
+        -nu * q + c * s(t) * (1 - u(t)) * w
+    ])
+
+# rhs function for the ODE governing the costate p
+# in our case this is the partial derivative of the non-maximized Hamiltonian in x
+def phi(t, p, u):
+    p1, p2 = p
+    return np.array([
+        (mu - b * prod(t) * u(t)) * p1,
+        nu * p2
+    ])
+
+def shoot(x0, p0_init, u_init, eps = 1e-5, max_iter = 100):
+
+    p0_curr = p0_init
+    u_curr = u_init
+    p0_last = p0_curr + 2 * eps
+    iteration_count = 0
+
+    while(np.abs(p0_last - p0_curr) > eps and iteration_count < max_iter):
+        sol_x_curr = solve_ivp(
+            f,
+            t_span=(0.0, T),
+            y0 = x0,
+            args = (u_curr,),
+            dense_output=True
+        )
+
+        sol_p_curr = solve_ivp(
+            phi,
+            t_span=(0.0, T),
+            y0 = p0_curr,
+            args = (u_curr,),
+        )
+
+        iteration_count += 1
